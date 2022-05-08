84e96e7f9a3b6f4948321a0f35215aca2d0ff548
Troy Wever <65945414+WeverTR@users.noreply.github.com>
2022-05-08 19:52:33 -0400

    Deploy Heroku

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { Canvas, Image } = require('canvas');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var APIRouter = require('./routes/api');
 
 var app = express();
 
@@ -23,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', APIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,8 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
 
 module.exports = app;
